5702081144c076bfdd1409c2cbc40813a816caa2
Jeff Sheffield <jeff.sheffield@gmail.com>
2015-03-23 18:28:28 -0500

    initial

---
+++ work_queues_02_new_task.py
@@ -0,0 +1,19 @@
+import pika 
+import sys
+
+connection_parms = pika.ConnectionParameters(host='localhost')
+connection = pika.BlockingConnection(connection_parms)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+
+channel.basic_publish(exchange='', routing_key='task_queue', body=message ,
+	                  properties=pika.BasicProperties(delivery_mode=2, # make the message persistant
+	                  ))
+
+print " [x] Sent %r" % ( message, )
+
+connection.close()
---
+++ work_queues_02_worker.py
@@ -0,0 +1,23 @@
+import pika 
+import sys
+import time
+
+connection_parms = pika.ConnectionParameters(host='localhost')
+connection = pika.BlockingConnection(connection_parms)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+
+print ' [*] Waiting for messages. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+	print " [x] Received %r" % ( body, )
+	time.sleep(body.count('.'))
+	print " [x] Done"
+	ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback, queue='task_queue') #no_ack=False
+
+channel.start_consuming()
